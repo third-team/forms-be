249fde3870f5c3a012f4e9d7363df81c3ab227a2
Yuriy Podnebesov <dartwader032@gmail.com>
2021-04-21 11:56:27 +0300

    added cors middleware

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const Koa = require('koa');
 const koaBodyParser = require('koa-bodyparser');
+const koaCors = require('@koa/cors');
 require('dotenv').config();
 const { router } = require('./api/routes/routes');
 
 const PORT = process.env.PORT || 3000;
 
 const app = new Koa();
+
+app.use(koaCors());
 
 app.use(koaBodyParser());
 
